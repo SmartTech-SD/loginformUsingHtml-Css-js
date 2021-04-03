ed699950a2df74f4388f58a62e7b5a003d7cb04b
Shubhankar Das <76389456+SmartTech-SD@users.noreply.github.com>
2021-04-03 13:50:02 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+
+function signup(){
+    document.getElementById("signup").style.display="block";
+    document.getElementById("login").style.display="none";
+}
+function login(){
+    document.getElementById("signup").style.display="none";
+    document.getElementById("login").style.display="block";
+}
